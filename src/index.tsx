e2be54caa9643167e9a35e33d7761ed37b83498c
kimneighbor <designkimdh@gmail.com>
2023-08-16 08:31:46 +0900

    counter/todos 추가

--- src/index.tsx
+++ src/index.tsx
@@ -3,23 +3,30 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { createStore } from 'redux';
-import counter from "./reducers";
+import {createStore} from 'redux';
+import rootReducer from "./reducers";
 
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+    document.getElementById('root') as HTMLElement
 );
 
-const store = createStore(counter);
+const store = createStore(rootReducer);
+
+store.dispatch({
+    type: 'ADD_TODO',
+    text: 'USE REDUX'
+})
+console.log('store.getState', store.getState());
+
 const render = () => root.render(
-  <React.StrictMode>
-    <App
-        value={store.getState()}
-        onIncrement={() => store.dispatch({ type: 'INCREMENT' })}
-        onDecrement={() => store.dispatch({ type: 'DECREMENT' })}
-    />
-  </React.StrictMode>
+    <React.StrictMode>
+        <App
+            value={store.getState()}
+            onIncrement={() => store.dispatch({type: 'INCREMENT'})}
+            onDecrement={() => store.dispatch({type: 'DECREMENT'})}
+        />
+    </React.StrictMode>
 );
 render();
 
