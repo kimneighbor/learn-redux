63a19157c7b60b83f27f7fe4717d4513b4024a0e
kimneighbor <designkimdh@gmail.com>
2023-08-16 08:59:50 +0900

    provider,useSelector_useDispatch

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
+import {useDispatch, useSelector} from "react-redux";
+import {RootState} from "./reducers";
 
 
 type Props = {
@@ -9,17 +11,40 @@ type Props = {
 }
 
 function App({value, onIncrement, onDecrement}: Props) {
-  return (
-    <div className="App">
-      {/*Clicked: {value} times*/}
-      <button onClick={onIncrement}>
-        +
-      </button>
-      <button onClick={onDecrement}>
-        -
-      </button>
-    </div>
-  );
+
+    const dispatch = useDispatch();
+    const counter = useSelector((state: RootState) => state.counter);
+    const todos: string[] = useSelector((state: RootState) => state.todos);
+    const [todoValue, setTodoValue] = useState('');
+    const hadleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setTodoValue(e.target.value);
+    }
+    const addTodo = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        dispatch({type: "ADD_TODO", text: todoValue});
+        setTodoValue('');
+
+    }
+    return (
+        <div className="App">
+            Clicked: {counter} times
+            <button onClick={onIncrement}>
+                +
+            </button>
+            <button onClick={onDecrement}>
+                -
+            </button>
+
+            <ul>
+                {todos.map((todo, index) => <li key={index}>{todo}</li>)}
+            </ul>
+
+            <form onSubmit={addTodo}>
+                <input type="text" value={todoValue} onChange={hadleChange}/>
+                <input type="submit"/>
+            </form>
+        </div>
+    );
 }
 
 export default App;
