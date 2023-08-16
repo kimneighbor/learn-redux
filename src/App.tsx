a733ee8a8cdaec0db4f73fd1f8698dec86895d54
kimneighbor <designkimdh@gmail.com>
2023-08-16 22:34:55 +0900

    middle-ware

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 import {useDispatch, useSelector} from "react-redux";
 import {RootState} from "./reducers";
+import axios from "axios";
 
 
 type Props = {
@@ -11,12 +12,28 @@ type Props = {
 }
 
 function App({value, onIncrement, onDecrement}: Props) {
-
     const dispatch = useDispatch();
     const counter = useSelector((state: RootState) => state.counter);
     const todos: string[] = useSelector((state: RootState) => state.todos);
+
     const [todoValue, setTodoValue] = useState('');
-    const hadleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+
+    useEffect(() => {
+        dispatch(fetchPosts())
+    }, [dispatch]);
+
+    const fetchPosts = ():any => {
+        return async function fetchPostsThunk(dispatch:any, getState:any) {
+            const response = await axios.get("https://jsonplaceholder.typicode.com/posts");
+            dispatch({type: "FETCH_POSTS", payload: response.data})
+        }
+    }
+
+
+
+
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         setTodoValue(e.target.value);
     }
     const addTodo = (e: React.FormEvent<HTMLFormElement>) => {
@@ -40,7 +57,7 @@ function App({value, onIncrement, onDecrement}: Props) {
             </ul>
 
             <form onSubmit={addTodo}>
-                <input type="text" value={todoValue} onChange={hadleChange}/>
+                <input type="text" value={todoValue} onChange={handleChange}/>
                 <input type="submit"/>
             </form>
         </div>
